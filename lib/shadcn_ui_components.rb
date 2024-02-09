1beda864fc2e36da8863893f7a32140a9b1a1519
nelsonhernandez20 <nelsonhernandez335@gmail.com>
2024-02-09 14:10:29 -0500

    config initial

---
+++ lib/shadcn_ui_components.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shadcn_ui_components/version"
+
+module ShadcnUiComponents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shadcn_ui_components/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ShadcnUiComponents
+  VERSION = "0.1.0"
+end
